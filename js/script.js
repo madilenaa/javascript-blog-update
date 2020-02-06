d164cd38bbff737631e7ed5e42c24ead774d2f0e
Magdalena Marchewka <madilenaa@gmail.com>
2020-02-06 17:41:02 +0100

    articles fixed

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
   const links = document.querySelectorAll('.titles a');
   console.log('links:', links);
 }); */
-
+{
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
@@ -35,8 +35,47 @@ const titleClickHandler = function(event){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+/* 6.4 */
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+const generateTitleLinks = function(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML='';
+
+  let html = '';
+
+/* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  for (let article of articles){
+      /* get the article id */
+    const articleId = article.getAttribute('id');
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId +'"><span>'+articleTitle +'</span></a></li>';
+    console.log(linkHTML);
+    titleList.insertAdjacentHTML('beforeend',linkHTML);
+    html = html + linkHTML;
+    console.log(html);
+  }
+    /* insert link into titleList */
+    titleList.innerHTML=html;
+
+    const links = document.querySelectorAll('.titles a');
+
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+      html = html + linkHTML;
+    }
+    titleList.innerHTML = html;
+  }
+  generateTitleLinks();
 }
